50f273660582571fc7142653031dcc8518a3de45
Dmitry Rakitin <dimrakitin@yandex.ru>
2023-05-22 14:23:32 +0300

    added solution

--- cpp_beginning/1_8_2d_array/1_8_2/src/main.cpp
+++ cpp_beginning/1_8_2d_array/1_8_2/src/main.cpp
@@ -5,14 +5,10 @@ int main()
 	int n { 0 };
 	std::cin >> n;
 
-	char arr[100][100];
+	int arr[100][100];
 	for (int i { 0 }; i < n; ++i) {
 		for (int j { 0 }; j < n; ++j) {
-			if (i == n/2 or i == j or i == n - j - 1 or j == n/2)
-				arr[i][j] = '*';
-
-			else
-				arr[i][j] = '.';
+			arr[i][j] = (i - j < 0) ? j - i : i - j;
 		}
 	}
 
